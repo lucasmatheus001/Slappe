010c467d984f0db0cd0c6d38f247bc9210ad205b
Lucas rocha <lucasmatheussm22@gmail.com>
2025-03-15 21:44:06 -0300

    Add active_storage to add files and posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:user_id, :content, images: [])
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many_attached :images
+  validate :validate_image_count
+
+  private
+  def validate_image_count
+   if images.length > 10
+     errors.add(:images, "Você pode adicionar no máximo 10 imagens.")
+   end
+  end
+end
